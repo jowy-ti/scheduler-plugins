0862d839fc1dec4dc7b59aaeb2ac4b5e849a3bb9
joel.gonzalez.jimenez <joel.gonzalez.jimenez@estudiantat.upc.edu>
2025-11-06 20:32:12 +0100

    unit tests

--- pkg/myscheduler/myscheduler.go
+++ pkg/myscheduler/myscheduler.go
@@ -7,7 +7,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/klog/v2"
+	klog "k8s.io/klog/v2"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
@@ -26,6 +26,8 @@ func New(_ context.Context, _ runtime.Object, h framework.Handle) (framework.Plu
 	return &MyScheduler{handle: h}, nil
 }
 
+// Constantes y estructuras de datos
+
 var _ framework.PreFilterPlugin = &MyScheduler{}
 var _ framework.FilterPlugin = &MyScheduler{}
 var _ framework.ScorePlugin = &MyScheduler{}
@@ -33,8 +35,6 @@ var _ framework.ScorePlugin = &MyScheduler{}
 const (
 	preFilterStateKey = "resources"
 )
-
-//////////////////////////////////////////////////////////////////////////////////
 
 // StateData
 type PreFilterState struct {
@@ -45,16 +45,16 @@ func (s *PreFilterState) Clone() framework.StateData {
 	return s
 }
 
-//////////////////////////////////////////////////////////////////////////////////
+// Etapas scheduling
 
+// Se cachea los recursos que pide el pod
 func (m *MyScheduler) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) (*framework.PreFilterResult, *framework.Status) {
-
 	// state.SetRecordPluginMetrics(true)
 
-	podResources := computePodResourceRequest(pod)
+	var podRequests *framework.Resource = computePodResourceRequest(pod)
 
-	preFilterState := &PreFilterState{
-		resources: *podResources,
+	var preFilterState *PreFilterState = &PreFilterState{
+		resources: *podRequests,
 	}
 
 	state.Write(preFilterStateKey, preFilterState)
@@ -66,40 +66,21 @@ func (m *MyScheduler) PreFilterExtensions() framework.PreFilterExtensions {
 	return nil
 }
 
-//////////////////////////////////////////////////////////////////////////////////
-
 func (m *MyScheduler) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
 
-	// Pulling pod requests
+	// pod requests
 	preFilterState, err := getPreFilterState(state)
 
 	if err != nil {
 		return framework.NewStatus(framework.Unschedulable, "Failed to read preFilterState from cycleState")
 	}
 
-	podRequests := &preFilterState.resources
-	nodeRequested := nodeInfo.Requested
-	nodeAllocatable := nodeInfo.Allocatable
+	var podRequests *framework.Resource = &preFilterState.resources
+	var nodeRequested *framework.Resource = nodeInfo.Requested
+	var nodeAllocatable *framework.Resource = nodeInfo.Allocatable
 
-	resourcesAvailable := subtractionResources(nodeAllocatable, nodeRequested)
-
-	if resourcesAvailable.MilliCPU < podRequests.MilliCPU {
-		return framework.NewStatus(framework.Unschedulable, "Insufficient CPU")
-	}
-
-	if resourcesAvailable.Memory < podRequests.Memory {
-		return framework.NewStatus(framework.Unschedulable, "Insufficient Memory")
-	}
-
-	for resourceName, amount := range podRequests.ScalarResources {
-		if resourcesAvailable.ScalarResources[resourceName] < amount {
-			return framework.NewStatus(framework.Unschedulable, fmt.Sprintf("Insufficient %s", resourceName.String()))
-		}
-	}
-	return framework.NewStatus(framework.Success)
+	return enoughNodeResources(nodeAllocatable, nodeRequested, podRequests)
 }
-
-//////////////////////////////////////////////////////////////////////////////////
 
 func (m *MyScheduler) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
 	nodeInfo, err := m.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
@@ -108,36 +89,32 @@ func (m *MyScheduler) Score(ctx context.Context, state *framework.CycleState, po
 		return 0, framework.NewStatus(framework.Error, fmt.Sprintf("getting node %q from Snapshot: %v", nodeName, err))
 	}
 
-	// Pulling pod requests
+	// pod requests
 	preFilterState, err := getPreFilterState(state)
 
 	if err != nil {
 		return 0, framework.NewStatus(framework.Error, "Failed to read preFilterState from cycleState")
 	}
 
-	podRequests := &preFilterState.resources
-	nodeRequested := nodeInfo.Requested
-	nodeAllocatable := nodeInfo.Allocatable
+	var podRequests *framework.Resource = &preFilterState.resources
+	var nodeRequested *framework.Resource = nodeInfo.Requested
+	var nodeAllocatable *framework.Resource = nodeInfo.Allocatable
 
-	// klog.V(0).Infof("%s CPU: %d ", nodeName, resourcesLeft.MilliCPU)
-	// klog.V(0).Infof("%s MEM: %d ", nodeName, resourcesLeft.Memory)
-	// klog.V(0).Infof("%s nvidia.com/mig-1g.6gb: %d ", nodeName, resourcesLeft.ScalarResources["nvidia.com/mig-1g.6gb"])
-	// klog.V(0).Infof("%s nvidia.com/mig-2g.12gb: %d ", nodeName, resourcesLeft.ScalarResources["nvidia.com/mig-2g.12gb"])
+	var nodeAvailable *framework.Resource = subtractionResources(nodeAllocatable, nodeRequested)
 
-	scoreCpuMem := scoreCpuMem(nodeAllocatable, nodeRequested, podRequests)
-	scoreGpu()
+	var score int64 = scoreCpuMem(nodeAllocatable, nodeRequested, nodeAvailable, podRequests) + scoreGpu(nodeAvailable)
 
-	klog.V(0).Infof("%s resCpuMem: %f", nodeName, scoreCpuMem)
+	klog.V(0).Infof("%s %s score: %d", pod.Name, nodeName, score)
 
-	return 50, framework.NewStatus(framework.Success)
+	return score, framework.NewStatus(framework.Success)
 }
 
 func (m *MyScheduler) ScoreExtensions() framework.ScoreExtensions {
 	return m
 }
 
-func (m *MyScheduler) NormalizeScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, scores framework.NodeScoreList) *framework.Status {
-	// framework.MaxNodeScore
+func (m *MyScheduler) NormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+	var MaxNodeScore float64 = float64(framework.MaxNodeScore)
 	var MaxScore int64 = math.MinInt64
 
 	for _, nodeScore := range scores {
@@ -147,15 +124,18 @@ func (m *MyScheduler) NormalizeScore(ctx context.Context, state *framework.Cycle
 	}
 
 	for i, nodeScore := range scores {
-		scores[i].Score = int64(100.0 - (100.0 * (float64(nodeScore.Score) / float64(MaxScore))))
+		scores[i].Score = int64(MaxNodeScore - (MaxNodeScore * (float64(nodeScore.Score) / float64(MaxScore))))
+		klog.V(0).Infof("%s %s Normalize: %d", pod.Name, scores[i].Name, scores[i].Score)
 	}
 	return framework.NewStatus(framework.Success)
 }
 
-//////////////////////////////////////////////////////////////////////////////////
+// Funciones auxiliares
 
+// Computa el total de recursos que solicita un pod
 func computePodResourceRequest(pod *v1.Pod) *framework.Resource {
-	result := &framework.Resource{}
+	var result *framework.Resource = &framework.Resource{}
+
 	for _, container := range pod.Spec.Containers {
 		result.Add(container.Resources.Requests)
 	}
@@ -173,8 +153,10 @@ func computePodResourceRequest(pod *v1.Pod) *framework.Resource {
 	return result
 }
 
+// Obtencion del StateData dentro del CycleState
 func getPreFilterState(cycleState *framework.CycleState) (*PreFilterState, error) {
 	stateData, err := cycleState.Read(preFilterStateKey)
+
 	if err != nil {
 		// preFilterState doesn't exist, likely PreFilter wasn't invoked.
 		return nil, fmt.Errorf("error reading %q from cycleState: %w", preFilterStateKey, err)
@@ -187,51 +169,85 @@ func getPreFilterState(cycleState *framework.CycleState) (*PreFilterState, error
 	return preFilterState, nil
 }
 
-func subtractionResources(Allocatable *framework.Resource, Requested *framework.Resource) *framework.Resource {
-	result := &framework.Resource{}
+// Recursos disponibles
+func subtractionResources(allocatable *framework.Resource, requested *framework.Resource) *framework.Resource {
+	var result *framework.Resource = &framework.Resource{}
 
-	result.MilliCPU = Allocatable.MilliCPU - Requested.MilliCPU
-	result.Memory = Allocatable.Memory - Requested.Memory
+	result.MilliCPU = allocatable.MilliCPU - requested.MilliCPU
+	result.Memory = allocatable.Memory - requested.Memory
 
-	if Allocatable.ScalarResources == nil {
+	if allocatable.ScalarResources == nil {
 		return result
 	}
 
 	result.ScalarResources = make(map[v1.ResourceName]int64)
 
-	for resourceNameAllocatable, quantityAllocatable := range Allocatable.ScalarResources {
-		result.ScalarResources[resourceNameAllocatable] = quantityAllocatable - Requested.ScalarResources[resourceNameAllocatable]
+	for resourceNameAllocatable, quantityAllocatable := range allocatable.ScalarResources {
+		result.ScalarResources[resourceNameAllocatable] = quantityAllocatable - requested.ScalarResources[resourceNameAllocatable]
 	}
 
 	return result
 }
 
-func scoreCpuMem(nodeAllocatable *framework.Resource, nodeRequested *framework.Resource, podRequests *framework.Resource) float64 {
-	var weightMem int64 = 1 << 20
+// Verifica si el nodo tiene recursos suficientes para el pod
+func enoughNodeResources(nodeAllocatable *framework.Resource, nodeRequested *framework.Resource, podRequests *framework.Resource) *framework.Status {
 
-	// Relative
-	memRel := float64(nodeRequested.Memory+podRequests.Memory) / float64(nodeAllocatable.Memory)
-	cpuRel := float64(nodeRequested.MilliCPU+podRequests.MilliCPU) / float64(nodeAllocatable.MilliCPU)
-	res := memRel - cpuRel
+	var resourcesAvailable *framework.Resource = subtractionResources(nodeAllocatable, nodeRequested)
 
-	if res < 0 {
-		res = -1.0 * res
+	if resourcesAvailable.MilliCPU < podRequests.MilliCPU {
+		return framework.NewStatus(framework.Unschedulable, "Insufficient CPU")
 	}
 
-	balanceCpuMem := 1.0 - (res / 2.0)
+	if resourcesAvailable.Memory < podRequests.Memory {
+		return framework.NewStatus(framework.Unschedulable, "Insufficient Memory")
+	}
 
-	// Absolute
-	resourcesAvailable := subtractionResources(nodeAllocatable, nodeRequested)
+	for resourceName, amount := range podRequests.ScalarResources {
+		if resourcesAvailable.ScalarResources[resourceName] < amount {
+			return framework.NewStatus(framework.Unschedulable, fmt.Sprintf("Insufficient %s", resourceName.String()))
+		}
+	}
 
-	memRes := resourcesAvailable.Memory / weightMem
-	weightedCpuMem := memRes + resourcesAvailable.MilliCPU
-
-	// Result
-	resCpuMem := float64(weightedCpuMem) * float64(balanceCpuMem)
-
-	return resCpuMem
+	return framework.NewStatus(framework.Success)
 }
 
-func scoreGpu() float64 {
+// Puntuación de la cpu y memoria
+func scoreCpuMem(nodeAllocatable *framework.Resource, nodeRequested *framework.Resource, nodeAvailable *framework.Resource, podRequests *framework.Resource) int64 {
+	const weightMem int64 = 1 << 20         // valor de la heuristica
+	const penalizationBalance float64 = 2.0 // mayor número penaliza menos el desbalance, menor penaliza más. Rango de valores posibles (1, inf) // valor de la heuristica
 
+	// Relative
+	var memRel float64 = float64(nodeRequested.Memory+podRequests.Memory) / float64(nodeAllocatable.Memory)
+	var cpuRel float64 = float64(nodeRequested.MilliCPU+podRequests.MilliCPU) / float64(nodeAllocatable.MilliCPU)
+	var relCpuMem float64 = memRel - cpuRel
+
+	if relCpuMem < 0 {
+		relCpuMem = -1.0 * relCpuMem
+	}
+
+	var balanceCpuMem float64 = 1.0 - (relCpuMem / penalizationBalance) // Intervalo de menos a más balanceado (0.5, 1)
+
+	// Absolute
+	var weightedCpuMem int64 = (nodeAvailable.Memory / weightMem) + nodeAvailable.MilliCPU
+
+	// Result
+	var resCpuMem float64 = float64(weightedCpuMem) / balanceCpuMem
+	// klog.V(0).Infof("Resources: %d     Balance: %f", weightedCpuMem, balanceCpuMem)
+	return int64(resCpuMem)
+}
+
+// Puntuación de la GPU
+func scoreGpu(nodeAvailable *framework.Resource) int64 {
+	const weightGpu int64 = 1 << 13 // valor de la heuristica
+	var resGpu int64 = 0
+
+	if nodeAvailable.ScalarResources == nil {
+		return resGpu
+	}
+
+	for _, quantityAvailable := range nodeAvailable.ScalarResources {
+		resGpu += quantityAvailable * weightGpu
+	}
+
+	return resGpu
 }
---
+++ pkg/myscheduler/myscheduler_test.go
@@ -0,0 +1,16 @@
+package myscheduler
+
+import (
+	"testing"
+	// "context"
+	// v1 "k8s.io/api/core/v1"
+	// clientsetfake "k8s.io/client-go/kubernetes/fake"
+	// framework "k8s.io/kubernetes/pkg/scheduler/framework"
+	// frameworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	// metrics "k8s.io/kubernetes/pkg/scheduler/metrics"
+	// testf "k8s.io/kubernetes/pkg/scheduler/testing/framework"
+)
+
+func TestNodeResourcesAllocatable(t *testing.T) {
+
+}
